47cd82b46155eee831865ce24192a2ffc80db269
hengnan <hengnan.ma@cooper.edu>
2024-10-06 23:15:49 -0400

    create input file function

---
+++ utils.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import json
+import torch
+
+def create_input_files():
+    """
+    Modifies dataset into input files for training and testing. 
+    :return:
+    """
